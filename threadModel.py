fb955fd5cc29c4aafbba364bcd18a5e91d493d61
ujeong1 <70083011+ujeong1@users.noreply.github.com>
2021-09-27 17:22:06 -0700

    Update threadModel.py

--- threadModel.py
+++ threadModel.py
@@ -24,8 +24,8 @@ class backgroundApp(QThread):
             CFRNet(self.command, self.dataset, self.experiments)
         # elif self.modelName == "Causal Effect Inference with Deep Latent-Variable Models (CEVAE)":
         #     CEVAE(self.command, self.dataset, self.experiments)
-        # elif self.modelName == "Bayesian Additive Regression Trees (BART)":
-        #     BART(self.command, self.dataset, self.experiments)
+        elif self.modelName == "Bayesian Additive Regression Trees (BART)":
+            BART(self.command, self.dataset, self.experiments)
         # elif self.modelName == "Causal Forests":
         #     CausalForests(self.command, self.dataset, self.experiments)
         # elif self.modelName == "Perfect Match":
@@ -44,6 +44,7 @@ class CFRNet:
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S-%f")
         config_dir = os.getcwd() + '\\CFRNet\\Results\\'+dataset.lower().strip()+str(experiments)+'\\results_' + timestamp + '\\'
         print("Output Directory: ", config_dir)
+        print(command)
         os.makedirs(config_dir)
         try:
             cfr_model.run(config_dir,command)
@@ -128,17 +129,28 @@ class SITE:
 #         overwrite = False
 #         filters = None
 #         cfr_evaluate(config_file, overwrite, filters=filters)
-
 class BART:
     def __init__(self, command, dataset, experiments):
+        command_list = command.split()
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S-%f")
-        config_dir = os.getcwd() + '\\CFRNet\\Results\\'+dataset.lower().strip()+str(experiments)+'\\results_' + timestamp + '\\'
+        config_dir = os.getcwd() + '\\BART\\Results\\'+dataset.lower().strip()+str(experiments)+'\\results_' + timestamp + '\\'
         print("Output Directory: ", config_dir)
         os.makedirs(config_dir)
         # Result = 5?
+        print(os.getcwd()+command_list[3])
+        print(os.getcwd() + command_list[5])
+        print(os.getcwd() + command_list[7])
+        print(os.getcwd() + command_list[9])
+        print(command_list[11])
+        print(command_list[13])
         try:
-            process = subprocess.call(["Rscript", sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]])
-        except Exception as e:
+            process = subprocess.call(["Rscript", (os.getcwd()+command_list[3]).strip(), (os.getcwd()+command_list[5]).strip(), (os.getcwd()+command_list[7]).strip(), (os.getcwd()+command_list[9]).strip(), command_list[11], command_list[13]])
+            if (process == 0):
+                print("Check the results in csv file")
+            else:
+                print("Some error occured. Try Again")
+                print(process)
+        except:
             with open(config_dir + 'error.txt', 'w') as errfile:
                 errfile.write(''.join(traceback.format_exception(*sys.exc_info())))
 # class CausalForests:

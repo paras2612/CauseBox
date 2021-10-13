9a7785637fc6a6757a4192c51e1ba40308a740bd
ujeong1 <70083011+ujeong1@users.noreply.github.com>
2021-10-12 20:33:29 -0700

    Update IHDP_uncompress.sh

--- DatasetScripts/IHDP_uncompress.sh
+++ DatasetScripts/IHDP_uncompress.sh
@@ -1,2 +1,2 @@
 tar -xzvf CFRNet/Dataset/IHDP/ihdp_npci_1-1000.train.npz.tar.gz CFRNet/Dataset/IHDP/
-tar -xvf PM/ihdp_npci_1-1000.train.npz.tar.gz PM/
+tar -xvzf PM/ihdp_npci_1-1000.train.npz.tar.gz PM/

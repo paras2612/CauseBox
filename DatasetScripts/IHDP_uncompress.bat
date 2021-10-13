87b1209af6b5953bbb71f6708d68101aae00aca2
ujeong1 <70083011+ujeong1@users.noreply.github.com>
2021-10-12 20:33:04 -0700

    Update IHDP_uncompress.bat

--- DatasetScripts/IHDP_uncompress.bat
+++ DatasetScripts/IHDP_uncompress.bat
@@ -1,2 +1,2 @@
-tar -xvzf CFRNet/Dataset/IHDP/ihdp_npci_1-1000.train.npz.tar.gz
-tar -xvzf PM/ihdp_npci_1-1000.train.npz.tar.gz
+tar -xvzf CFRNet\Dataset\IHDP\ihdp_npci_1-1000.train.npz.tar.gz
+tar -xvzf PM\ihdp_npci_1-1000.train.npz.tar.gz
